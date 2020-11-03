1f463eca90dd8a7c31ee0a4ee8ac41790e5c8bf8
Wayne Hofstra <wayne.hofstra@hva.nl>
2020-11-03 14:10:09 +0100

    Optimalized Grid Placement

--- Assets/Scripts/GridGeneration/MeshGridPlacement.cs
+++ Assets/Scripts/GridGeneration/MeshGridPlacement.cs
@@ -10,7 +10,6 @@ public class MeshGridPlacement : MonoBehaviour
     Vector3[] gridPosition = new Vector3[2];
     float offset;
     float hexAngle;
-    bool getOffset;
 
     void Start()
     {
@@ -19,23 +18,21 @@ public class MeshGridPlacement : MonoBehaviour
 
     void InitGrid()
     {
-        getOffset = false;
         hexAngle = Mathf.Sin(60 * Constants.Math.DEGREE_TO_RADIAN);
         gridPosition[0] = new Vector3(transform.position.x - (hexAngle * (_gridLength.x - 0.5f)),
                                       transform.position.y, transform.position.z + (Mathf.Pow(hexAngle, 2) *
                                      (_gridLength.y - 1)));
         offset = Mathf.Cos(60 * Constants.Math.DEGREE_TO_RADIAN) * hexAngle * 2;
 
-        for (int i = 0; i < _gridLength.y; i++) {
-            for (int j = 0; j < _gridLength.x; j++)
-            {
-                gridPosition[1] = new Vector3(gridPosition[0].x + (j * 2 * hexAngle), transform.position.y,
-                                                  gridPosition[0].z - (i * Mathf.Pow(Mathf.Sin(60 * Constants.Math.DEGREE_TO_RADIAN), 2) * 2));
+        for (int i = 0; i < (_gridLength.x * _gridLength.y); i++)
+        {
+            gridPosition[1] = new Vector3(gridPosition[0].x + ((i % _gridLength.x) * 2 * hexAngle), transform.position.y,
+                                          gridPosition[0].z - (Mathf.FloorToInt(i / _gridLength.y) * Mathf.Pow(Mathf.Sin(60 * Constants.Math.DEGREE_TO_RADIAN), 2) * 2));
 
-                if (getOffset) gridPosition[1].x += offset;
-                Instantiate(_mesh, gridPosition[1], transform.rotation, transform);
+            if ((i % (_gridLength.x * 2)) >= _gridLength.x && (i % (_gridLength.x * 2)) < (_gridLength.x * 2)) {
+                gridPosition[1].x += offset;
             }
-            getOffset = !getOffset;
+            Instantiate(_mesh, gridPosition[1], transform.rotation, transform);
         }
     }
 }
